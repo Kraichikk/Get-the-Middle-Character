ce74e0a6eb14d385b4a66003c52ee86f0b2c7f7a
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 12:49:00 +0200

    Add project files.

---
+++ Get the Middle Character/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Get_the_Middle_Character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMiddle("pilika"));
+        }
+        public static string GetMiddle(string s)
+        {
+            string str = "";
+            char[] cArr = s.ToCharArray();
+            if (s.Length % 2 != 0)
+            {
+                str += cArr[s.Length / 2 ];
+                return str;
+            }
+            else
+            {
+                str += cArr[cArr.Length  / 2 - 1];
+                str += cArr[cArr.Length / 2 ];
+                return str;
+            }
+        }
+    }
+}
